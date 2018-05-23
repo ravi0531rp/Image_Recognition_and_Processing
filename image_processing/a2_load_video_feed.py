2555f9f6c3d3e89c67d1f09cdff54b742c7fd5c1
ravi0531rp <34860386+ravi0531rp@users.noreply.github.com>
2018-05-23 09:41:45 +0530

    Update a2_load_video_feed.py

--- image_processing/a2_load_video_feed.py
+++ image_processing/a2_load_video_feed.py
@@ -7,7 +7,7 @@ out = cv2.VideoWriter('output.avi', fourcc, 20.0, (640, 480))
 while True:
     ret, frame = cap.read()  # true/false , the feed
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    out.write(frame)
+    out.write(frame)  # writing out
     cv2.imshow('frame', frame)
     cv2.imshow('gray', gray)
     if cv2.waitKey(5) & 0xFF == ord('q'):
